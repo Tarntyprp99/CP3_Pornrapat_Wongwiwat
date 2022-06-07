16192eab20f340e68c7ea39ae493d05e6089bbae
Tarntyprp99 <106254378+Tarntyprp99@users.noreply.github.com>
2022-06-07 21:18:33 +0700

    Create Exercise4_Pornrapat_W.py

---
+++ Exercise4_Pornrapat_W.py
@@ -0,0 +1,11 @@
+Foundation_English = 85
+General_Business = 73
+Introduction_to_Computer_systems = 91
+Computer_Programming = 80
+
+print("--- Your Score ---")
+print("Foundation English :",Foundation_English)
+print("General Business :",General_Business)
+print("Introduction to Computer Systems :",Introduction_to_Computer_systems)
+print("Computer Programming :",Computer_Programming)
+print("-------------------")
